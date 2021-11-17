fa91b7cfab8ea2040ddcac8e8b135b79ef17bcc6
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-11-17 14:09:38 -0300

    70% completed

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -1,17 +1,23 @@
-import { User } from "../../model/User";
-import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { User } from '../../model/User'
+import { IUsersRepository } from '../../repositories/IUsersRepository'
 
 interface IRequest {
-  name: string;
-  email: string;
+  name: string
+  email: string
 }
 
 class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const userExists = this.usersRepository.findByEmail(email)
+    if (userExists) {
+      throw new Error('User already exists')
+    }
+    const user = this.usersRepository.create({ email, name })
+
+    return user
   }
 }
 
-export { CreateUserUseCase };
+export { CreateUserUseCase }
--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,9 +6,18 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const all = this.listAllUsersUseCase.execute()
+    const { user_id } = request.headers
+    try {
+      const all = this.listAllUsersUseCase.execute({ user_id } as {
+        user_id: string
+      })
 
-    return response.json(all)
+      return response.json(all)
+    } catch (err) {
+      return response.status(400).json({
+        error: err.message
+      })
+    }
   }
 }
 
